050d9a609827967aa8f2f8386532f54151a2c94f
losen <18616272151@163.com>
2017-08-31 21:10:57 +0800

    tkinter自带的测试窗口

---
+++ tk_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/31 21:05
+# @Author  : 李振华
+
+import tkinter
+
+
+# tkinter自带的测试窗口
+def application():
+    tkinter._test()
+
+
+if __name__ == "__main__":
+    # 调用函数
+    application()
