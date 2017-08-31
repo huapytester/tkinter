770046e12920615545658d75a1ab99be6e5f6f7a
losen <18616272151@163.com>
2017-08-31 20:54:47 +0800

    第一个tkinter的界面

---
+++ tk001.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/31 20:44
+# @Author  : 李振华
+
+import tkinter
+
+
+# 第一个tkinter的界面
+# application [ˌæplɪ'keɪʃ(ə)n] 应用程序
+def application():
+    # 创建一个窗口对象
+    top = tkinter.Tk()
+    # 设置主循环
+    top.mainloop()
+
+
+if __name__ == "__main__":
+    # 调用函数
+    application()
